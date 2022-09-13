987f83e4275c1a8548b114e4708689ed0702e241
Alexis Mata WebDev <alexedmata@gmail.com>
2022-09-12 22:59:21 -0400

    first commit

---
+++ database/migrations/2022_09_13_023359_clientes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('RazaoSocial', 25);
+            $table->string('NomeFantasia', 50);
+            $table->string('CNPJ', 16)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
